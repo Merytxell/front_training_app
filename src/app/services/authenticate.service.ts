42cc91c259b09e39934d6f42da3ee3e72c609e1c
Claire AVILA <avila.claire@gmail.com>
2024-03-08 13:03:43 +0100

    ajout du crypatage et décrypatage

--- src/app/services/authenticate.service.ts
+++ src/app/services/authenticate.service.ts
@@ -3,6 +3,7 @@ import { User } from '../model/user.model';
 import { cryptoService } from '../service/AES/crypto-service.';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,13 +14,45 @@ private users = [
 {email:'claire@gmail.com' ,password:'1234', roles:['ADMIN','USER']},
 {email:'toto@gmail.com' ,password:'1234', roles:['USER']},
 ];
-  constructor() { this.connectedUser = this.getUser() }
+  constructor( private cryptoService : cryptoService) { this.connectedUser = this.getUser() }
   
 
   //trouver si l'user existe en comparant ce qu'il saisit aux infos dans users
   existingUser(email : string, password : string) : boolean{
     const user = this.users.find(u => u.email === email && u.password === password);
     if(user){
+      this.saveUser(user);
+      return true;
+    }
+    return false;
+  }
+
+ /**
+  * Methode pour crypter les données users
+  * @param email 
+  * @param password 
+  * @returns 
+  */
+  encryptedUser (email : string, password : string) : boolean {
+    const user = this.users.find ( u => u.email === email && u.password === password);
+    if (user){
+      const cryptedEmail = this.cryptoService.encrypt(user.password);
+      const cryptedPassword = this.cryptoService.encrypt(user.email);
+      this.saveUser(user)
+      return true;
+    }
+    return false;
+
+  }
+
+/**
+ * Methode pour décrypter les données users
+ */
+  decryptedUser (email : string, password : string) : boolean  {
+    const user = this.users.find (u => u.email === email && u.password === password);
+    if (user){
+      const decryptedEmail = this.cryptoService.decrypt(user.password);
+      const decryptedPassword = this.cryptoService.decrypt(user.email);
       this.saveUser(user);
       return true;
     }
